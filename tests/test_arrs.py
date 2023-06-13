7702305bc2a0b81d1512bca193982d0f24aec275
vink77 <vink.av@mail.ru>
2023-06-14 01:28:03 +0700

    unittest_1

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,12 +1,13 @@
 import unittest
 from utils import arrs
 
-
 class TestArrs(unittest.TestCase):
 
     def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 3)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
+        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+        #self.assertEqual(arrs.get([], 0, "test"), "test")
+        with self.assertRaises(IndexError):
+            arrs.get([], 0)
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
